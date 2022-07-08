0afc8178e39d89a51c93292a0b8b4805b1d6e797
Melih Gündüz <melih.gunduz@hotmail.com>
2022-07-08 21:33:20 +0300

    photos sorted by date

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,28 +2,83 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Fotoğraf seçimi</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <div class="center">
+        <ion-button size="large" @click="fotoSec">
+          <ion-icon :icon="camera"></ion-icon> &nbsp; Kamera
+        </ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import IonComps from "@/shared/IonComps";
+import {Camera, CameraResultType} from '@capacitor/camera'
+import {db, st, auth} from "@/main";
+import {Geolocation} from "@capacitor/geolocation"
+import {v4} from 'uuid'
+import {camera} from 'ionicons/icons';
+import {useRouter} from "vue-router";
+import firebase from "firebase/compat/app";
 
-export default  defineComponent({
+export default {
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
-</script>
+  components: {...IonComps},
+
+
+  setup() {
+    const router = useRouter()
+
+    const fotoSec = async () => {
+      const fotograf: any = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64
+      })
+      if (fotograf.base64String) {
+        const kullanici: any = auth.currentUser;
+
+        const guid = v4();
+
+        const dosyaYolu = `${kullanici.uid}/resimler/${guid}.${fotograf.format}`
+
+        const storageRef = st.ref()
+        await storageRef.child(dosyaYolu).putString(fotograf.base64String, 'base64')
+
+        const url = await storageRef.child(dosyaYolu).getDownloadURL() //yayınlanmış halinin url i
+
+        const konum = await Geolocation.getCurrentPosition();
+        const tarih = firebase.firestore.FieldValue.serverTimestamp()
+
+        await db.collection('kullanicilar').doc(kullanici.uid).collection('fotograflar').add({
+          fotograf: url,
+          konum: {enlem: konum.coords.latitude, boylam: konum.coords.longitude},
+          yuklemeZamani: tarih,
+        }).then(() => {
+
+          console.log('Fotoğraf başarıyla yüklendi')
+
+          //router.push("/tabs/tab2")
+          location.assign("/tabs/tab2")
+        })
+      }
+
+    }
+    return {camera, fotoSec}
+
+  }
+
+}</script>
+
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,71 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Fotoğraflar</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+    <ion-content v-if="yukleniyor">
+      <div class="center">
+        <ion-spinner color="primary">
+        </ion-spinner>
+      </div>
+    </ion-content>
+    <ion-content v-else>
+      <div v-if="fotolar.length>0">
+        <ion-card v-for="(foto,index) in fotolar" :key="index">
+          <ion-img :src="foto"></ion-img>
+        </ion-card>
+      </div>
+      <div v-else>Henüz fotoğraf paylaşmadınız</div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import IonComp from "@/shared/IonComps";
+import {reactive, toRefs} from "vue";
+import {auth, db,} from '../main'
 
-export default defineComponent({
+export default ({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {...IonComp},
+
+  setup() {
+    const state = reactive({
+      fotolar: [] as string[],
+      yukleniyor: false
+    })
+
+    const fotolarGetir = async () => {
+      state.yukleniyor = true;
+      const kullanici: any = auth.currentUser;
+      const snap = await db.collection('kullanicilar').doc(kullanici.uid).collection('fotograflar')
+          .orderBy('yuklemeZamani', "desc").get()
+
+
+      if (!snap.empty) {
+        snap.docs.forEach(doc => {
+          const data = doc.data();
+
+          if (data.fotograf) {
+            state.fotolar.push(data.fotograf)
+          }
+        })
+      }
+
+      state.yukleniyor = false
+    }
+    fotolarGetir()
+
+    return {...toRefs(state)}
+  }
 });
 </script>
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+}
+</style>
